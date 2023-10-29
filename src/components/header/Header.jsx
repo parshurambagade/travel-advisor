f49b741c294e7bc605a23044e42af4cbd8d8aae1
Parshuram <parshurambagade855@gmail.com>
2023-10-29 22:22:33 +0530

    Search bar added

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,18 +1,24 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import "./header.scss";
+import SearchBar from './SearchBar';
 
-const Header = () => {
+const Header = ( {setCoordinates}) => {
   const { loginWithRedirect, logout, isAuthenticated } = useAuth0();
   return (
     <div className="navbar">
       <div className="logo">
         <h2>Travel Advisor</h2>
       </div>
+
+      <div className="searchbar">
+        <SearchBar setCoordinates={setCoordinates} />
+      </div>
+      
       <div className="nav-buttons">
-        {isAuthenticated ? <button className="btn logout-btn" onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>
+        {isAuthenticated ? <button className="logout-btn" onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>
       Log Out
-    </button> : <button className="btn login-btn" onClick={() => loginWithRedirect()}>Log In</button>}
+    </button> : <button className="login-btn" onClick={() => loginWithRedirect()}>Log In</button>}
       
       
       </div>
